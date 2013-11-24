7cd5fdaa4cf1211bbea31731670ad10b5f3ce547
giantneco <giantneco@gmail.com>
2013-11-24 23:10:50 +0900

    Aded Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,17 @@
 module Main where
+
+import Text.Parsec
+import Text.Parsec.String
+
+data FortranTopLevel 
+  = Program
+  -- | Subroutine
+  -- | Function
+  -- | Module     -- TODO
+    deriving Show
+
+fortranCode :: Parser [FortranTopLevel]
+fortranCode = undefined
+
+fortranTopLevel :: Parser FortranTopLevel
+fortranTopLevel = undefined

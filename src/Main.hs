808a7ae9d7afeae083ef2e2d03a3d779f13a2ab0
giantneco <giantneco@gmail.com>
2013-12-09 21:05:57 +0900

    Modify main to parse input files

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,21 @@
 module Main where
 
-main = putStrLn "not yet"
+import Text.Parsec
+import HFortran.AST
+import HFortran.Parser
+
+import System.IO (readFile)
+import System.Environment (getArgs)
+import Data.List (isPrefixOf)
+
+doParseAndShow :: String -> IO ()
+doParseAndShow file = 
+  do src <- readFile file
+     case parse program file src of
+       Right ast -> print ast
+       Left x -> print x
+
+main = do 
+    files <- getArgs
+    mapM_ doParseAndShow files
+
---
+++ test/testdata/program-continue.f90
@@ -0,0 +1,4 @@
+      program main
+        continue
+      end program main
+
